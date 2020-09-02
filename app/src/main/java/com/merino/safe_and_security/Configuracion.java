9bc0f3896388b63554958a0b9b15332c6e8cbd8b
thundebolt <nicolasmerino321@gmail.com>
2020-09-01 21:04:35 -0400

    Primer commit creado proyecto android

---
+++ app/src/main/java/com/merino/safe_and_security/Configuracion.java
@@ -0,0 +1,25 @@
+package com.merino.safe_and_security;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Configuracion extends AppCompatActivity {
+    private Button btnvolver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracion);
+        btnvolver = findViewById(R.id.btnvolver);
+
+    }
+
+    public void volver(View View){
+        Intent volver = new Intent(this,MainActivity.class);
+        startActivity(volver);
+    }
+}
--- app/src/main/java/com/merino/safe_and_security/MainActivity.java
+++ app/src/main/java/com/merino/safe_and_security/MainActivity.java
@@ -2,13 +2,36 @@ package com.merino.safe_and_security;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    // Creamos la variable que hara referencia a widget
+    private TextView txttitle;
+    private Button btnpanic;
+    private Button btncon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Realizar las referencias
+        txttitle = findViewById(R.id.txttitle);
+        btncon = findViewById(R.id.btncon);
+        btnpanic = findViewById(R.id.btnpanic);
+    }
+    public void Saludar(View View ) {
+        Toast.makeText(this,"Cambiaste de color",Toast.LENGTH_LONG).show();
+        txttitle.setTextColor(Color.BLUE);
+
+    }
+    public void configuracion(View View){
+        Intent configuracion = new Intent(this,Configuracion.class);
+        startActivity(configuracion);
     }
 }
