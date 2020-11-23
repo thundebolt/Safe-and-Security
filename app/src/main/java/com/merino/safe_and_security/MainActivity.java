2bc8347c2b353fe66d6f885fe6796cfa3180c55f
thundebolt <nicolasmerino321@gmail.com>
2020-11-23 03:09:06 -0300

    Se conecta con firebase y funciona el inicio sesion

--- app/src/main/java/com/merino/safe_and_security/MainActivity.java
+++ app/src/main/java/com/merino/safe_and_security/MainActivity.java
@@ -1,5 +1,6 @@
 package com.merino.safe_and_security;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,17 +10,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
     // Creamos la variable que hara referencia a widget
 
-   Button btninicio,btncancelar,btnregistro;
-   EditText txtusuario,txtpass;
-
-
-
+    Button btninicio, btncancelar, btnregistro;
+    EditText email, pass;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -27,11 +32,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Realizar las referencias
+        mAuth = FirebaseAuth.getInstance();
         btncancelar = findViewById(R.id.btncancelar);
         btninicio = findViewById(R.id.btninicio);
         btnregistro = findViewById(R.id.btnregistro);
-        txtusuario = findViewById(R.id.txtusuario);
-        txtpass = findViewById(R.id.txtpass);
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.pass);
 
         btnregistro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,11 +49,61 @@ public class MainActivity extends AppCompatActivity {
         btncancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txtpass.setText("");
-                txtusuario.setText("");
+                pass.setText("");
+                email.setText("");
             }
         });
+        btninicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validar(view);
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void validar(View view) {
+
+
+        if (email.getText().length() <= 0) {
+            email.setError("Ingrese su nombre");
+        } else if (pass.getText().length() <= 0) {
+            pass.setError("ingrese una contraseña");
         }
 
 
+        mAuth.signInWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            // Log.d(TAG, "signInWithEmail:success");
+                            Toast.makeText(MainActivity.this, "Bienvenido " , Toast.LENGTH_LONG).show();
+                            Intent interor = new Intent(MainActivity.this, Botonpanic.class);
+                            startActivity(interor);
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //   Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //   updateUI(null);
+                        }
+                    }
+
+                });
+
     }
+
+
+}
--- app/src/main/java/com/merino/safe_and_security/Registro.java
+++ app/src/main/java/com/merino/safe_and_security/Registro.java
@@ -1,32 +1,42 @@
 package com.merino.safe_and_security;
 
+        import androidx.annotation.NonNull;
         import androidx.appcompat.app.AppCompatActivity;
 
         import android.content.Intent;
         import android.os.Bundle;
-        import android.util.Patterns;
+
         import android.view.View;
         import android.widget.Button;
         import android.widget.EditText;
         import android.widget.Toast;
 
-        import java.util.regex.Pattern;
+        import com.google.android.gms.tasks.OnCompleteListener;
+        import com.google.android.gms.tasks.Task;
+
+        import com.google.firebase.auth.AuthResult;
+        import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.auth.FirebaseUser;
+
+
 
 public class Registro extends AppCompatActivity {
     EditText txtusuario, txtpass, txtemail;
     Button btnregistro, btnvolver;
+    private FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_registro);
 
         btnvolver = findViewById(R.id.btnvolver);
         btnregistro = findViewById(R.id.btnregistro);
-        txtusuario = findViewById(R.id.txtusuario);
+        txtusuario = findViewById(R.id.email);
         txtpass = findViewById(R.id.txtcontraseña);
-        txtemail = findViewById(R.id.txtemail);
+        txtemail = findViewById(R.id.email);
 
         btnvolver.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,7 +50,7 @@ public class Registro extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                validardatos(view);
+                registrarse(view);
 
 
 
@@ -48,33 +58,68 @@ public class Registro extends AppCompatActivity {
         });
 
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser)
+    }
 
+    public  void registrarse(View view){
+
+
+        if (txtpass.getText().length() <=0){
+            txtpass.setError("Ingrese su contraseña");
+        }else if (txtpass.getText().length() <=8){
+            txtpass.setError("la contraseña debe ser mayor a 8 letras");
+
+        }
+        else if (txtemail.getText().length() <= 0 ){
+            txtemail.setError("Ingrese su email");
+        }
+        else {
+
+            mAuth.createUserWithEmailAndPassword( txtemail.getText().toString(),txtpass.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(Registro.this,"ESTAS REGISTRADO :3",Toast.LENGTH_LONG).show();
+                                Intent interor = new Intent(Registro.this,MainActivity.class);
+                                startActivity(interor);
+                              //  updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                            //    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(Registro.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                               // updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+
+
+        }
+
+
+
+    }
     public void validardatos(View view) {
         String usuario = txtusuario.getText().toString();
         String password = txtpass.getText().toString();
         String email = txtemail.getText().toString();
 
 
-        if (usuario.length() == 0){
-            Toast.makeText(this, "Debes ingresar un usuario",Toast.LENGTH_LONG).show();
-        }
-        else if(usuario.length() < 3){
-            Toast.makeText(this,"El nombre de usuario debe ser mayor a 3 letras",Toast.LENGTH_LONG).show();
-        }
-        if (password.length() <8){
-            Toast.makeText(this,"La contraseña debe ser mayor a 8 caracteres",Toast.LENGTH_LONG).show();
-        }
-        else if (email.length() == 0){
-            Toast.makeText(this,"El email esta vacio",Toast.LENGTH_LONG).show();
-        }
-        else {
-            Toast.makeText(this,"ESTAS REGISTRADO :3",Toast.LENGTH_LONG).show();
-            Intent interor = new Intent(Registro.this,Botonpanic.class);
-            startActivity(interor);
-
-        }
 
 
     }
 
+
 }
+
